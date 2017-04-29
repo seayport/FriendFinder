5d88e864cbaa5eb954331a6437508022116b4c5d
Sandy Seay <seaysandra@bellsouth.net>
2017-04-29 08:27:42 -0400

    heroku tweaks

--- server.js
+++ server.js
@@ -14,17 +14,20 @@ var PORT = process.env.PORT || 3000;
 
 // Sets up the Express app to handle data parsing
 //BodyParser makes it possible for our server to interpret data sent to it.
-app.use( "/app", express.static( "app") );
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.text());
 app.use(bodyParser.json({ type: "application/vnd.api+json" }));
  
+app.use(express.static(__dirname + "/app")); 
 // ================================================================================
 // ROUTER
 // The below points our server to a series of "route" files.
 // These routes give our server a "map" of how to respond when users visit or request data from various URLs.
 // ================================================================================
+// Routes
+
+// =============================================================
 
 require("./app/routing/apiRoutes.js")(app);
 require("./app/routing/htmlRoutes.js")(app);
