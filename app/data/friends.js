144623d1f2f8a38e38c8153206e3dbc9d8bb0eba
Sandy Seay <seaysandra@bellsouth.net>
2017-04-29 07:45:32 -0400

    added more friends

--- app/data/friends.js
+++ app/data/friends.js
@@ -6,9 +6,77 @@
 // ===============================================================================
 var friendsArray = [
 {
-    "name": "Ahmed",
-    "photo": "http://1.bp.blogspot.com/-6nITuLchAng/VNUUezC2nXI/AAAAAAAAIsE/tZhXOnDtC3o/s1600/0_480_640_0_70_-News-20110311113302_FaceBook.jpg",
-    "scores": [
+"name": "Vivvy Seay",
+"photo": "http://cdn.akc.org/akcdoglovers/Pomeranian_hero.jpg",
+"scores": [
+"3",
+"2",
+"2",
+"5",
+"2",
+"2",
+"2",
+"2",
+"2",
+"3"
+],
+"match": 0
+},
+{
+"name": "Vincent Van Gogh",
+"photo": "https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcQM7HhEaZ_ItLCfY-9ZU0qBK5PSFpg9uiWZZegicIQG5uRtHx005Q",
+"scores": [
+"3",
+"2",
+"2",
+"5",
+"2",
+"2",
+"2",
+"2",
+"2",
+"3"
+],
+"match": 0
+},
+{
+"name": "George Burdell",
+"photo": "https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcQNW1xv1Ba84iZJwC3mQTn39gIxHgXirDU6JPrHOqowW9GK8BWrMEfEIw",
+"scores": [
+"3",
+"2",
+"2",
+"5",
+"2",
+"2",
+"2",
+"2",
+"2",
+"3"
+],
+"match": 0
+},
+{
+"name": "Yogi Bear",
+"photo": "https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcQTRZiwfQoYyKK0qJP9Q4OXq2_L_tr-nu--LEfHosLYD_eykTlQTw",
+"scores": [
+"3",
+"2",
+"2",
+"5",
+"2",
+"2",
+"2",
+"2",
+"2",
+"3"
+],
+"match": 0
+},
+{
+ "name": "Ahmed",
+ "photo": "http://1.bp.blogspot.com/-6nITuLchAng/VNUUezC2nXI/AAAAAAAAIsE/tZhXOnDtC3o/s1600/0_480_640_0_70_-News-20110311113302_FaceBook.jpg",
+ "scores": [
             "5",
             "1",
             "4",
@@ -19,9 +87,8 @@ var friendsArray = [
             "5",
             "4",
             "1"
-        ]
-
-	}
+       ]
+}
 ];
     
 // Note how we export the array. This makes it accessible to other files using require.
